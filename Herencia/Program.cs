99d6e62c9fa7bbeb88088b9186be8ebb9dd55d7e
Demonear <126305470+FernandoYael@users.noreply.github.com>
2023-03-21 19:32:32 +0000

    Hola

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -12,12 +12,27 @@
     }
 }
 
+class Empleado:Persona
+{
+    public string NumEmpleado{get; set;}
+
+    public Empleado(string nombre, string numEmpleado):base(nombre)
+    {
+        NumEmpleado = numEmpleado;
+    }
+    public void Imprime()
+    {
+        Console.WriteLine($"{this.NumEmpleado}{this.Nombre}");
+    }
+}
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         Persona p1= new Persona("Alicia");
+        Empleado e1= new Empleado ("Alicia","005")
         p1.Imprime();
+        e1.Imprime();
     }
 }
