532fe6386e7387eb815bb68df2a82e5d76121678
Demonear <126305470+FernandoYael@users.noreply.github.com>
2023-03-21 19:17:50 +0000

    Hola

---
+++ Herencia/Program.cs
@@ -0,0 +1,23 @@
+﻿class Persona
+{
+    public string Nombre{get; set;}
+
+    public Persona(string nombre)
+    {
+        Nombre = nombre;
+    }
+    public void Imprime()
+    {
+        Console.WriteLine(Nombre);
+    }
+}
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Persona p1= new Persona("Alicia");
+        p1.Imprime();
+    }
+}
